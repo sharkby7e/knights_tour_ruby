0305411a3e7a60d348ca8877eb466068407eba78
sharkby7e <sgquin@gmail.com>
2024-04-05 14:44:23 -0400

    cleanup squares controller

--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -1,25 +1,25 @@
 class SquaresController < ApplicationController
   def index
-    # @located_square = Square.find_by(location_params)
-    # x = location_params[:location][:x]
-    # y = location_params[:location][:y]
+    @squares = Square.all
 
-    Square.update_all(has_knight: false)
+    @squares.update_all(has_knight: false)
+
     if params[:location].present?
-      x = params[:location][:x] 
-      y = params[:location][:y] 
-      @located_square = Square.find_by(x: x, y: y)
+      x = params[:location][:x]
+      y = params[:location][:y]
+      @located_square = Square.find_by(x: x, y:y)
       @located_square.update!(has_knight: true)
     end
 
-    @squares = []
-
-    Square.all.each do |square|
-      @squares << square
-    end
   end
 
   def show
     @square = Square.find(params[:id])
   end
+  #
+  # private
+  #
+  # def location_params
+  #   params.permit(:location).permit([:x, :y])
+  # end
 end
