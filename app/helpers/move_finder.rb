695b9ee4ede891fbf2b554d4986f311e391139ea
sharkby7e <sgquin@gmail.com>
2024-04-09 23:52:29 -0400

    test drive move finder

--- app/helpers/move_finder.rb
+++ app/helpers/move_finder.rb
@@ -1,5 +1,5 @@
 class MoveFinder
-  POSSIBLE_MOVES = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]].freeze
+  MOVE_SET = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]].freeze
 
   attr_accessor :x, :y
 
@@ -8,6 +8,17 @@ class MoveFinder
     @y = square.y
   end
 
+  def legal_moves
+    move_candidates.select do |candidate|
+      (candidate[0]).positive? && candidate[0] <= 8 && (candidate[1]).positive? && candidate[1] <= 8
+    end
+  end
+
+  def move_candidates
+    MOVE_SET.map { |move| move(move) }
+  end
+
   def move(move)
+    [x + move[0], y + move[1]]
   end
 end
--- spec/helpers/move_finder_spec.rb
+++ spec/helpers/move_finder_spec.rb
@@ -1,10 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe MoveFinder, type: :helper do
-  it 'initializes with a square with accessible coordinates' do
-    square = create(:square)
+  let(:square) { create(:square) }
+  let(:finder) { MoveFinder.new(square:) }
 
-    finder = MoveFinder.new(square:)
+  it 'initializes with a square with accessible coordinates' do
     expect(finder.x).to eq 1
+  end
+
+  describe '#move_candidates' do
+    it 'provides an array of the possible moves given a square' do
+      expect(finder.move_candidates).to eq [[2, 3], [3, 2], [3, 0], [2, -1], [0, -1], [-1, 0], [-1, 2], [0, 3]]
+    end
+  end
+
+  describe '#legal_moves' do
+    it 'filters out moves that are off the board' do
+      expect(finder.legal_moves).to eq [[2, 3], [3, 2]]
+    end
+
+    it 'filters out moves with a coord greater than 8' do
+      expect(finder).to receive(:move_candidates).and return [[2, 8], [9, 8], [2, 9]]
+      expect(finder.legal_moves).to eq [[2, 8]]
+    end
+
+    it 'filters out moves with a coord less than 1' do
+      expect(finder).to receive(:move_candidates).and return [[0, 6], [-1, 3], [2, -1]]
+      expect(finder.legal_moves).to eq [[2, 8]]
+    end
   end
 end
