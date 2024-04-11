036317463ea66569628f8e262e02d99178c99439
sharkby7e <sgquin@gmail.com>
2024-04-11 12:25:31 -0400

    color the legal squares green

--- spec/controllers/squares_controller_spec.rb
+++ spec/controllers/squares_controller_spec.rb
@@ -8,13 +8,14 @@ RSpec.describe SquaresController, type: :request do
     end
 
     it 'assigns the legal squares' do
-      Square.create!(x: 1, y: 1)
+      located_square = Square.create!(x: 1, y: 1)
       legal_square = Square.create!(x: 2, y: 3)
       another_legal_square = Square.create!(x: 3, y: 2)
 
       params = { location: { x: 1, y: 1 } }
       get(squares_path(params))
 
+      expect(assigns[:located_square]).to eq located_square
       expect(assigns[:legal_squares]).to eq [legal_square, another_legal_square]
     end
   end
